b45fb87fc418baaccfe4147024406affca85000e
Tiago Maluta <tiago.maluta@gmail.com>
2014-07-06 20:12:58 +0000

    full path to json file

--- res/js/dndTree.js
+++ res/js/dndTree.js
@@ -1,5 +1,5 @@
 // Get JSON data
-treeJSON = d3.json("../../json/volpato.json", function(error, treeData) {
+treeJSON = d3.json("http://maluta.github.io/res/json/volpato.json", function(error, treeData) {
 
     // Calculate total nodes, max label length
     var totalNodes = 0;
